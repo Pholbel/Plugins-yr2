8f4bef74b95fc2cade488436c65c04bc1b639f39
PaulHolbel3 <pholbel@ucsd.edu>
2018-10-05 16:38:04 -0700

    added completed plugins forlder and WIP

---
+++ Work in Progress/OBEPPlugIn/OBEPPlugIn/WebParse.cs
@@ -0,0 +1,97 @@
+﻿using HtmlAgilityPack;
+using PlugIn4_5;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OBEPPlugIn
+{
+    public class WebParse
+    {
+        public string Expiration { get; private set; }
+        public SanctionType Sanction { get; private set; }
+
+        private string TdPair = "<tr><td>{0}</td><td>{1}</td></tr>";
+        private string TdSingle = "<td>{0}</td>";
+        private RegexOptions RegOpt = RegexOptions.IgnoreCase | RegexOptions.Singleline;
+
+        public WebParse()
+        {
+            Expiration = String.Empty;
+            Sanction = SanctionType.None;
+        }
+
+        public Result<string> Execute(IRestResponse response)
+        {
+            try
+            {
+                /*
+                MatchCollection physicians = Regex.Matches(response.Content, "id=\"ctl00_cphMain_rptrPhysician_ctl00_lblLicense\"", RegOpt);
+                MatchCollection assistants = Regex.Matches(response.Content, "id=\"ctl00_cphMain_rptrAssistant_ctl00_lblLicense\"", RegOpt);
+
+                if (physicians.Count > 1 || assistants.Count > 1 || (physicians.Count > 0 && assistants.Count > 0))
+                {
+                    return Result<string>.Failure(ErrorMsg.MultipleProvidersFound);
+                }
+                else if (Regex.Match(response.Content, "No Physicians Match That License", RegOpt).Success 
+                    && Regex.Match(response.Content, "No Physician Assistants Match That License", RegOpt).Success)
+                {
+                    return Result<string>.Failure(ErrorMsg.NoResultsFound);
+                }
+                else // Returned successful query
+                {
+                    CheckLicenseDetails(response.Content);
+                    return ParseResponse(response.Content);
+                }*/
+                return ParseResponse(response.Content);
+            }
+            catch (Exception e)
+            {
+                return Result<string>.Exception(e);
+            }
+        }
+
+        private void CheckLicenseDetails(string response)
+        {
+            Match exp = Regex.Match(response, "Expiration date: </span>( |\t|\r|\v|\f|\n)*<span.*?>(?<EXP>.*?)</span>", RegOpt);
+            if (exp.Success)
+            {
+                Expiration = exp.Groups["EXP"].ToString();
+            }
+
+            //Does not support sanctions
+
+        }
+
+        private Result<string> ParseResponse(string response)
+        {
+            MatchCollection fields = Regex.Matches(response, "(?<=(id=\"ctl.*\">)).*(?=</s)");
+            List<string> headers = new List<string>(new string[] {"First Name", "Middle Name", "Last Name", "License Type", "License Number", "Title", "Effective Date", "Expiration Date", "Status", "Finding"});
+
+            if (fields.Count > 0)
+            {
+                StringBuilder builder = new StringBuilder();
+
+                for (int idx=0;idx<fields.Count;idx++)
+                {
+                    string header = headers[idx % headers.Count];
+                    string text = fields[idx].ToString();
+
+                    builder.AppendFormat(TdPair, header, text);
+                    builder.AppendLine();
+                }
+
+
+                return Result<string>.Success(builder.ToString());
+            }
+            else // Error parsing table
+            {
+                return Result<string>.Failure(ErrorMsg.CannotAccessDetailsPage);
+            }
+        }
+    }
+}
