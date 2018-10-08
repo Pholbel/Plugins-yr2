405a118b11dd4597827008c4360864ace3a3541d
Michael Hollingworth <mhollingworthl@gmail.com>
2018-10-08 10:50:18 -0700

    IBPL plugin completed. Ready for verification.

---
+++ Work in Progress/IBPLPlugin/IBPLPlugin/WebParse.cs
@@ -0,0 +1,87 @@
+﻿using HtmlAgilityPack;
+using PlugIn4_5;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IBPLPlugIn
+{
+    public class WebParse
+    {
+        public string Expiration { get; private set; }
+        public SanctionType Sanction { get; private set; }
+
+        private string TdPair = "<tr><td>{0}</td><td>{1}</td></tr>";
+        private string TdSingle = "<td>{0}</td>";
+        private RegexOptions RegOpt = RegexOptions.IgnoreCase | RegexOptions.Singleline;
+
+        public WebParse()
+        {
+            Expiration = String.Empty;
+            Sanction = SanctionType.None;
+        }
+
+        public Result<string> Execute(IRestResponse response)
+        {
+            try
+            {
+                CheckLicenseDetails(response.Content);
+                return ParseResponse(response.Content);
+            }
+            catch (Exception e)
+            {
+                return Result<string>.Exception(e);
+            }
+        }
+
+        private void CheckLicenseDetails(string response)
+        {
+            //Get license dates
+            MatchCollection exp = Regex.Matches(response, "<td align=\"center\">(\\d*/\\d*/\\d*)</td>", RegOpt);
+
+            //There are two dates in the form data: one for date issued and one for expiration,
+            //but they do not have id tags to differentiate them
+            if (exp.Count == 2)
+                Expiration = exp[1].Groups[1].ToString();
+
+            //Get sanction status
+            Match sanction = Regex.Match(response, "<td style=\"padding-left:5px;\">Disciplinary Action</td>\n*\\s*<td style=\"padding-left:5px;\">\n*\\s*(N)", RegOpt);
+
+            //The regex specifically looks for no sanctions
+            if (sanction.Success)
+                Sanction = SanctionType.None;
+            else
+                Sanction = SanctionType.Red;
+        }
+
+        private Result<string> ParseResponse(string response)
+        {
+            //Headers for all the relevant details
+            MatchCollection headers = Regex.Matches(response, "<th>([\\w\\s]*)</th>", RegOpt);
+
+            //Values of the fields
+            MatchCollection fields = Regex.Matches(response, "<td align=\"center\">([\\d\\w\\s\\./]*)</td>", RegOpt);
+
+            if (fields.Count > 0)
+            {
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < fields.Count; i++)
+                {
+                    builder.AppendFormat(TdPair, headers[i].Groups[1].ToString(), fields[i].Groups[1].ToString());
+                    builder.AppendLine();
+                }
+
+                return Result<string>.Success(builder.ToString());
+            }
+            else // Error parsing table
+            {
+                return Result<string>.Failure(ErrorMsg.CannotAccessDetailsPage);
+            }
+        }
+    }
+}
---
+++ Work in Progress/IBPLPlugin/IBPLPlugin/WebSearch.cs
@@ -0,0 +1,108 @@
+﻿using PlugIn4_5;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IBPLPlugIn
+{
+    public class WebSearch
+    {
+
+        private RegexOptions RegOpt = RegexOptions.IgnoreCase | RegexOptions.Singleline;
+        private Provider provider { get; set; }
+
+        public WebSearch(Provider _provider)
+        {
+            this.provider = _provider;
+        }
+
+        public Result<IRestResponse> Execute()
+        {
+            try
+            {
+                return Search();
+            }
+            catch (Exception ex)
+            {
+                return Result<IRestResponse>.Exception(ex);
+            }
+        }
+
+        private Result<IRestResponse> Search()
+        {
+
+            // PARAMETERS AND COOKIES WE WILL GET WITH FIRST GET
+            List<RestResponseCookie> allCookies = new List<RestResponseCookie>();
+            string baseUrl = "https://ibplicense.iowa.gov/PublicPortal/Iowa/IBPL/publicsearch";
+
+            //Set up security protocol
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            //Set up client and request
+            RestClient client = new RestClient(baseUrl + "/publicsearch.jsp");
+            RestRequest request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            
+            //Store new cookies
+            allCookies.AddRange(response.Cookies);
+
+            //Check that we accessed the site
+            if (response.StatusCode != HttpStatusCode.OK)
+                return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSite);
+
+            //FORMING NEW POST WITH OUR PARAMS
+            request = new RestRequest(Method.POST);
+
+            //We perform the search using license numbers and last names
+            request.AddParameter("licenseNumber", provider.LicenseNumber);
+            request.AddParameter("lastName", provider.LastName);
+
+            //Add cookies to our request
+            foreach (RestResponseCookie c in allCookies)
+                request.AddCookie(c.Name, c.Value);
+
+            response = client.Execute(request);
+
+            //Store new cookies
+            allCookies.AddRange(response.Cookies);
+
+            //Check that we accessed the site
+            if (response.StatusCode != HttpStatusCode.OK)
+                return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSite);
+
+            MatchCollection providerList = Regex.Matches(response.Content, "(<tr class=\"FormtableData\">)", RegOpt);
+
+            //Check if we have multiple providers
+            if (providerList.Count == 0)
+                return Result<IRestResponse>.Failure(ErrorMsg.NoResultsFound);
+            else if (providerList.Count > 1)
+                return Result<IRestResponse>.Failure(ErrorMsg.MultipleProvidersFound);
+
+            //Get the directory the details for this provider are in
+            Match fields = Regex.Match(response.Content, "id=\"licenseCheckBox_1\"\\svalue=\"(\\d+)\"", RegOpt);
+            string folderRsn = fields.Groups[1].ToString();
+            client = new RestClient(baseUrl + "/publicsearch_detail.jsp?folderRsn=" + folderRsn);
+            request = new RestRequest(Method.POST);
+
+            //Add cookies to our request
+            foreach (RestResponseCookie c in allCookies)
+                request.AddCookie(c.Name, c.Value);
+
+            response = client.Execute(request);
+
+            //Get new cookies
+            allCookies.AddRange(response.Cookies);
+
+            //Check result
+            if (response.StatusCode == HttpStatusCode.OK)
+                return Result<IRestResponse>.Success(response);
+            else
+                return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSearchResultsPage);
+        }
+    }
+}
