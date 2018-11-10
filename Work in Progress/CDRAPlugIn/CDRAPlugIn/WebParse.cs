eecadd74812390094ce5ac3e58587bcf85b5dbb6
M3L6H <mhollbusiness@gmail.com>
2018-11-09 20:22:48 -0800

    Testing complete. Awaiting verification.

--- Work in Progress/CDRAPlugIn/CDRAPlugIn/WebParse.cs
+++ Work in Progress/CDRAPlugIn/CDRAPlugIn/WebParse.cs
@@ -41,7 +41,7 @@ namespace CDRAPlugIn
         private void CheckLicenseDetails(string response)
         {
             //Ensure we get the expiration date of the license
-            Match exp = Regex.Match(response, "<td>\\w*\\.\\d+</td>(<td>(<font color=green>)?[\\w ]+(</font>)?</td>){3}(<td>\\d+/\\d+/\\d+</td>){2}<td>(?<date>\\d+/\\d+/\\d+)</td>", RegOpt);
+            Match exp = Regex.Match(response, "<td>\\w*\\.\\d+</td>(<td>(<font color=\\w+>)?[&;\\w ]+(</font>)?</td>){3}(<td>((\\d+/\\d+/\\d+)|(&nbsp;))</td>){2}<td>(?<date>\\d+/\\d+/\\d+)</td>", RegOpt);
 
             //Set the expiration date to the expiration date of the latest one
             if (exp.Success)
@@ -61,7 +61,9 @@ namespace CDRAPlugIn
         {
             //Headers
             MatchCollection headers = Regex.Matches(response, "<th scope=\"col\">(?<header>[\\w ]+)</th>", RegOpt);
-            MatchCollection values = Regex.Matches(response, "<td>(<font color=green>)?(?<value>[,/\\.\\w ]*)(&nbsp;)?(</font>)?</td>", RegOpt);
+            MatchCollection values = Regex.Matches(response, "<td>(<font color=\\w+>)?((?<value>[-&,/\\.\\w ]+)|(&nbsp;)?(<br>)*)+(</font>)?</td>", RegOpt);
+            Match sanctionHeaders = Regex.Match(response, "Board/Program Actions[-;:/\"=<>/\\w\\s]+?(<th scope=\"col\">(?<header>[\\w ]+)</th>)+", RegOpt);
+            Match sanctionValues = Regex.Match(response, "Board/Program Actions[-;:/\"=<>/\\w\\s]+?((<td>(?<value>[-&,/\\.\\w]*)(&nbsp;)?</td>)+[=\"<>/\\s\\w]*?(<td>(?<value>[-&,/\\.\\w ]*)(&nbsp;)?</td>)*)+\\s*</tr>\\s*</tbody>", RegOpt);
 
             if (values.Count > 0)
             {
@@ -70,7 +72,24 @@ namespace CDRAPlugIn
 
                 for (int i = 0; i < headers.Count; i++)
                 {
-                    builder.AppendFormat(TdPair, headers[i].Groups["header"].Value, values[i].Groups["value"].Value);
+                    //We handle sanctions in a separate loop
+                    if (headers[i].Groups["header"].Value == "Case Number")
+                        break;
+
+                    string value = "";
+
+                    for (int j = 0; j < values[i].Groups["value"].Captures.Count; j++)
+                        value += values[i].Groups["value"].Captures[j].Value + " ";
+
+                    builder.AppendFormat(TdPair, headers[i].Groups["header"].Value, value);
+                    builder.AppendLine();
+                }
+
+                int headerCount = sanctionHeaders.Groups["header"].Captures.Count;
+
+                for (int i = 0; i < sanctionValues.Groups["value"].Captures.Count; i++)
+                {
+                    builder.AppendFormat(TdPair, sanctionHeaders.Groups["header"].Captures[i % headerCount], sanctionValues.Groups["value"].Captures[i]);
                     builder.AppendLine();
                 }
 
