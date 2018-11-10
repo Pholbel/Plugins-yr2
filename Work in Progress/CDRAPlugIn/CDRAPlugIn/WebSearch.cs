56a517b6946e2552dc94d6d9da844050abd48646
M3L6H <mhollbusiness@gmail.com>
2018-11-09 18:36:20 -0800

    Completed search.

--- Work in Progress/CDRAPlugIn/CDRAPlugIn/WebSearch.cs
+++ Work in Progress/CDRAPlugIn/CDRAPlugIn/WebSearch.cs
@@ -41,14 +41,15 @@ namespace CDRAPlugIn
 
             // PARAMETERS AND COOKIES WE WILL GET WITH FIRST GET
             List<RestResponseCookie> allCookies = new List<RestResponseCookie>();
-            string baseUrl = "https://apps.colorado.gov/dora/licensing/Lookup/LicenseLookup.aspx";
+            string baseUrl = "https://apps.colorado.gov/dora/licensing/Lookup";
 
             //Set up security protocol
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
             //Set up client and request
             RestClient client = new RestClient(baseUrl);
-            RestRequest request = new RestRequest(Method.GET);
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)";
+            RestRequest request = new RestRequest("LicenseLookup.aspx", Method.GET);
             IRestResponse response = client.Execute(request);
             
             //Store new cookies
@@ -59,7 +60,25 @@ namespace CDRAPlugIn
                 return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSite);
 
             //Forming new post request with our parameters
-            request = new RestRequest(Method.POST);
+            request = new RestRequest("LicenseLookup.aspx", Method.POST);
+
+            //Headers
+            request.AddHeader("X-MicrosoftAjax", "Delta=true");
+            request.AddHeader("X-Requested-With", "XMLHttpRequest");
+            //request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+
+            //Add cookies to our request
+            foreach (RestResponseCookie c in allCookies)
+                request.AddCookie(c.Name, c.Value);
+
+            //Event headers
+            string event_target = "", event_argument = "", view_state = "", view_state_generator = "", async = "";
+            event_target = "ctl00$MainContentPlaceHolder$ucLicenseLookup$UpdtPanelGridLookup";
+            event_argument = "1";
+            async = "true";
+
+            GetViewStates(ref view_state, ref view_state_generator, response);
+            SetViewStates(event_target, event_argument, view_state, view_state_generator, async, ref request);
 
             //We perform the search using license numbers and/or names
             string licNo = provider.LicenseNumber;
@@ -67,8 +86,8 @@ namespace CDRAPlugIn
             //Licenses can have sub types
             Tuple<string, string> type = GetLicenseType(ref licNo);
 
-        /* BEGIN ADD PARAMETERS */
-            request.AddParameter("ctl100$ScriptManager1", "ctl00$MainContentPlaceHolder$ucLicenseLookup$UpdtPanelGridLookup|ctl00$MainContentPlaceHolder$ucLicenseLookup$UpdtPanelGridLookup");
+            /* BEGIN ADD PARAMETERS */
+            request.AddParameter("ctl00$ScriptManager1", "ctl00$MainContentPlaceHolder$ucLicenseLookup$UpdtPanelGridLookup|ctl00$MainContentPlaceHolder$ucLicenseLookup$UpdtPanelGridLookup");
 
             //Search details
             request.AddParameter(PREFIX + "ctl03$ddCredPrefix", type.Item1);
@@ -86,69 +105,18 @@ namespace CDRAPlugIn
             request.AddParameter("ctl00$OutsidePlaceHolder$ucLicenseDetailPopup$ResizeLicDetailPopupID_ClientState", "0,0");
             /* END ADD PARAMETERS */
 
-            //Event headers
-            string event_target = "", event_argument = "", event_validation = "", view_state = "", view_state_generator = "", async = "";
-            event_target = "ctl00$MainContentPlaceHolder$ucLicenseLookup$UpdtPanelGridLookup";
-            event_argument = "1";
-            async = "true";
-
-            GetViewStates(ref event_validation, ref view_state, ref view_state_generator, response);
-            SetViewStates(event_target, event_argument, event_validation, view_state, view_state_generator, async, ref request);
-
-            //if (type != "")
-            //{
-            //    request.AddParameter(PREFIX + "ddlbLicenseType", type);
-            //    request.AddParameter(PREFIX + "txtLicenseNumber", licNo);
-            //    request.AddParameter(PREFIX + "rbtnSearch", "1");
-            //} else
-            //{
-            //    //Switch to name mode
-            //    request.AddParameter(PREFIX + "txtLicenseNumber", "");
-            //    request.AddParameter(PREFIX + "ddlbLicenseType", "CD");
-            //    request.AddParameter(PREFIX + "rbtnSearch", "2");
-            //    GetViewStates(ref event_target, ref event_argument, ref event_validation, ref view_state, ref view_state_generator, ref last_focus, response);
-            //    SetViewStates("_ctl7$rbtnSearch$1", event_argument, event_validation, view_state, view_state_generator, last_focus, ref request);
-            //    //Add cookies to our request
-            //    foreach (RestResponseCookie c in allCookies)
-            //        request.AddCookie(c.Name, c.Value);
-            //    response = client.Execute(request);
-
-            //    //Store new cookies
-            //    allCookies.AddRange(response.Cookies);
-
-            //    //Check that we accessed the site
-            //    if (response.StatusCode != HttpStatusCode.OK)
-            //        return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSearchForm);
-
-            //    //Forming new post request with our parameters
-            //    request = new RestRequest(Method.POST);
-
-            //    //Get new parameters
-            //    request.AddParameter(PREFIX + "txtLastName", provider.LastName);
-            //    request.AddParameter(PREFIX + "txtFirstName", provider.FirstName);
-            //    request.AddParameter(PREFIX + "rbtnSearch", "2");
-            //}
-
-            //Add cookies to our request
-            foreach (RestResponseCookie c in allCookies)
-                request.AddCookie(c.Name, c.Value);
-
-            client.BaseUrl = new Uri(baseUrl + "&step=1");
-
+            //Site uses a modal window with an async request
             response = client.Execute(request);
 
             //Store new cookies
             allCookies.AddRange(response.Cookies);
 
             //Check that we accessed the site
-            if (response.StatusCode != HttpStatusCode.OK && response.StatusCode != HttpStatusCode.Redirect)
+            if ((response.StatusCode != HttpStatusCode.OK && response.StatusCode != HttpStatusCode.Redirect)||
+                Regex.Match(response.Content, "ErrorPage.aspx").Success)
                 return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSite);
 
-            //If we searched by license number, site automatically redirects us to details page
-            //if (type != "")
-            //    return Result<IRestResponse>.Success(response);
-
-            MatchCollection providerList = Regex.Matches(response.Content, "_ctl7_grdSearchResults__ctl[\\d]_Hyperlink1", RegOpt);
+            MatchCollection providerList = Regex.Matches(response.Content, "DisplayLicenceDetail\\(&#39;(?<Detail>[;\\w ]+)&#39;\\)");
 
             //Check if we have multiple providers
             if (providerList.Count > 1)
@@ -159,15 +127,13 @@ namespace CDRAPlugIn
                 return Result<IRestResponse>.Failure(ErrorMsg.NoResultsFound);
 
             //Navigate to the details page
-            Match m = Regex.Match(response.Content, "&amp;(?<id>id=[\\d]+)&amp;(?<type>lictype=[\\w]+)\"><");
-            client.BaseUrl = new Uri($"{baseUrl}&step=2&{m.Groups["id"].Value}&{m.Groups["type"].Value}");
-
-            //Forming new get request with our parameters
-            request = new RestRequest(Method.GET);
+            request = new RestRequest("licensedetail.aspx", Method.GET);
 
             //Add cookies to our request
             foreach (RestResponseCookie c in allCookies)
                 request.AddCookie(c.Name, c.Value);
+
+            request.AddQueryParameter("id", providerList[0].Groups["Detail"].Value);
 
             response = client.Execute(request);
 
@@ -187,18 +153,16 @@ namespace CDRAPlugIn
             return new Tuple<string, string>(type, subtype);
         }
 
-        void GetViewStates(ref string validation, ref string state, ref string generator, IRestResponse response)
+        void GetViewStates(ref string state, ref string generator, IRestResponse response)
         {
-            validation = Regex.Match(response.Content, "id=\"__EVENTVALIDATION\"\\s*value=\"([\\w\\+/=]*)", RegOpt).Groups[1].Value;
             generator = Regex.Match(response.Content, "id=\"__VIEWSTATEGENERATOR\"\\s*value=\"([\\w]*)", RegOpt).Groups[1].Value;
             state = Regex.Match(response.Content, "id=\"__VIEWSTATE\"\\s*value=\"([\\w\\+/=]*)", RegOpt).Groups[1].Value;
         }
 
-        void SetViewStates(string target, string argument, string validation, string state, string generator, string async, ref RestRequest request)
+        void SetViewStates(string target, string argument, string state, string generator, string async, ref RestRequest request)
         {
             request.AddParameter("__EVENTTARGET", target);
             request.AddParameter("__EVENTARGUMENT", argument);
-            request.AddParameter("__EVENTVALIDATION", validation);
             request.AddParameter("__VIEWSTATE", state);
             request.AddParameter("__VIEWSTATEGENERATOR", generator);
             request.AddParameter("__ASYNCPOST", async);
