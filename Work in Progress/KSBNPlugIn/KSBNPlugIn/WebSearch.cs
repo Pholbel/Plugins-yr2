ed1847df56e53609a47269a8ec0c758a0ed45ee6
timferido <timferido@csu.fullerton.edu>
2018-11-20 08:13:04 -0800

    Setup KSBN

---
+++ Work in Progress/KSBNPlugIn/KSBNPlugIn/WebSearch.cs
@@ -0,0 +1,180 @@
+﻿using PlugIn4_5;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KSBNPlugIn
+{
+    public class WebSearch
+    {
+        private RegexOptions RegOpt = RegexOptions.IgnoreCase | RegexOptions.Singleline;
+        private Provider provider { get; set; }
+
+        public WebSearch(Provider _provider)
+        {
+            this.provider = _provider;
+        }
+
+        public Result<IRestResponse> Execute()
+        {
+            try
+            {
+                return Search();
+            }
+            catch (Exception ex)
+            {
+                return Result<IRestResponse>.Exception(ex);
+            }
+        }
+
+        private Result<IRestResponse> Search()
+        {
+
+            // PARAMETERS AND COOKIES WE WILL GET WITH FIRST GET
+            List<RestResponseCookie> allCookies = new List<RestResponseCookie>();
+            string viewState = "";
+            string viewStateGenerator = "";
+            string eventValidation = "";
+            string lastFocus = "";
+            string eventTarget = "";
+            string eventArgument = "";
+            string baseUrl = "http://cvl.KSBN.ca.gov/";
+
+            //GET PARAMETERS AND COOKIES
+            RestClient client = new RestClient(baseUrl+"SearchPage.aspx");
+            RestRequest request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            
+            allCookies.AddRange(response.Cookies);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                GetViewStates(ref viewState, ref viewStateGenerator, ref eventValidation, ref lastFocus, ref eventTarget, ref eventArgument, response);
+            }
+            else
+            {
+                return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSite);
+            }
+
+            //FORMING NEW POST WITH OUR PARAMS
+            request = new RestRequest(Method.POST);
+
+            string _param_prefix = "ctl00$ContentPlaceHolderMiddleColumn$";
+
+            request.AddParameter("__EVENTTARGET", eventTarget);
+            request.AddParameter("__EVENTARGUMENT", eventArgument);
+            request.AddParameter("__LASTFOCUS", lastFocus);
+            request.AddParameter("__VIEWSTATE", viewState);
+            request.AddParameter("__VIEWSTATEGENERATOR", viewStateGenerator);
+            request.AddParameter("__EVENTVALIDATION", eventValidation);
+            request.AddParameter(_param_prefix + "ddCertType", "0");
+            request.AddParameter(_param_prefix + "CVLSearch", "rdoLastFirst");
+            request.AddParameter(_param_prefix + "txtLastName", provider.LastName);
+            request.AddParameter(_param_prefix + "txtFirstName", provider.FirstName);
+            request.AddParameter(_param_prefix + "txtLastNameStart", "");
+            request.AddParameter(_param_prefix + "btnSearch2", "Search");
+            
+            foreach (var c in allCookies)
+            {
+                request.AddCookie(c.Name, c.Value);
+            }
+
+            response = client.Execute(request);
+
+            //LAND HO!
+
+            //REFILL THE COOKIE JAR
+            allCookies.AddRange(response.Cookies);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                GetViewStates(ref viewState, ref viewStateGenerator, ref eventValidation, ref lastFocus, ref eventTarget, ref eventArgument, response);
+            }
+            else { return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSite); }
+
+            //CHECK IF WE HAVE MULTIPLE PROVIDERS
+
+            MatchCollection providerList = Regex.Matches(response.Content, "(?<=cert.*\">).*(?=</a>)", RegOpt);
+            HashSet<string> providerHash = new HashSet<string>();
+
+            foreach (var p in providerList)
+            {
+                providerHash.Add(p.ToString());
+            }
+
+            if (providerHash.Count == 0)
+            {
+                return Result<IRestResponse>.Failure(ErrorMsg.NoResultsFound);
+            }
+            else if (providerHash.Count == 1)
+            {
+                Match fields = Regex.Match(response.Content, "(?<QUERY>\"DetailPage.aspx?.*?\\\")", RegOpt);
+                string detailQuery = fields.Groups["QUERY"].ToString();
+                detailQuery = Regex.Replace(detailQuery, "\"", "", RegOpt);
+                client = new RestClient(baseUrl + detailQuery);
+                request = new RestRequest(Method.GET);
+
+                foreach (var c in allCookies)
+                {
+                    request.AddCookie(c.Name, c.Value);
+                }
+
+                response = client.Execute(request);
+
+                allCookies.AddRange(response.Cookies);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return Result<IRestResponse>.Success(response);
+                }
+                else
+                {
+                    return Result<IRestResponse>.Failure(ErrorMsg.CannotAccessSearchResultsPage);
+                }
+            }
+            else
+            {
+                return Result<IRestResponse>.Failure(ErrorMsg.MultipleProvidersFound);
+            }
+            
+        }
+
+        private void GetViewStates(ref string viewState, ref string viewStateGenerator, ref string eventValidation, ref string lastFocus, ref string eventTarget, ref string eventArgument, IRestResponse response)
+        {
+            Match m = Regex.Match(response.Content, "id=\"__VIEWSTATE\" value=\"(?<VIEW>.*?)\"", RegOpt);
+            if (m.Success)
+            {
+                viewState = m.Groups["VIEW"].ToString();
+            }
+            m = Regex.Match(response.Content, "id=\"__VIEWSTATEGENERATOR\" value=\"(?<VIEWGEN>.*?)\"", RegOpt);
+            if (m.Success)
+            {
+                viewStateGenerator = m.Groups["VIEWGEN"].ToString();
+            }
+            m = Regex.Match(response.Content, "id=\"__EVENTVALIDATION\" value=\"(?<EVENT>.*?)\"", RegOpt);
+            if (m.Success)
+            {
+                eventValidation = m.Groups["EVENT"].ToString();
+            }
+            m = Regex.Match(response.Content, "id=\"__LASTFOCUS\" value=\"(?<EVENT>.*?)\"", RegOpt);
+            if (m.Success)
+            {
+                lastFocus = m.Groups["EVENT"].ToString();
+            }
+            m = Regex.Match(response.Content, "id=\"__EVENTTARGET\" value=\"(?<EVENTTAR>.*?)\"", RegOpt);
+            if (m.Success)
+            {
+                eventTarget = m.Groups["EVENTTAR"].ToString();
+            }
+            m = Regex.Match(response.Content, "id=\"__EVENTARGUMENT\" value=\"(?<EVENTARG>.*?)\"", RegOpt);
+            if (m.Success)
+            {
+                eventArgument = m.Groups["EVENTARG"].ToString();
+            }
+        }
+    }
+}
