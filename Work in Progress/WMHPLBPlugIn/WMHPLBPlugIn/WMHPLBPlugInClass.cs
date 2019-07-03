0125ec2db144c19d2d7dc96639d72e48368a5ad5
timferido <timferido@csu.fullerton.edu>
2019-07-03 16:38:48 -0700

    added google sheets api

---
+++ Work in Progress/WMHPLBPlugIn/WMHPLBPlugIn/WMHPLBPlugInClass.cs
@@ -0,0 +1,106 @@
+﻿using PlugIn4_5;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace WMHPLBPlugIn
+{
+    /// <summary>
+    /// EchoPSV Plugin for Wyoming
+    /// https://docs.google.com/spreadsheets/d/1HdBP5KsCmmy1VuYTU8LZZI7RKUZ1aW4sFRjsILPYWXE/edit#gid=0
+    /// </summary>
+    public class PlugInClass : IPlugIn
+    {
+        private RegexOptions RegOpt = RegexOptions.IgnoreCase | RegexOptions.Singleline;
+
+        public override string Fetch(DataRow dr)
+        {
+            Initialize(dr, true, new SPMConfig() { AcceptAllCertificatePolicy = true, Expect100Continue = false });
+            string message = String.Empty;
+            string output = String.Empty;
+
+            Result<string> fields = Validate();
+
+            if (fields.IsValid)
+            {
+                WebSearch webSearch = new WebSearch(provider);
+                Result<ValueRange> response = webSearch.Execute();
+
+                if (response.IsValid)
+                {
+                    WebParse webParse = new WebParse();
+                    Result<string> parseResult = webParse.Execute(response.Value);
+
+                    if (parseResult.IsValid)
+                    {
+                        HandleModes(webParse);
+
+                        output = parseResult.Value;
+                    }
+                    else
+                    {
+                        message = parseResult.Message;
+                    }
+                }
+                else
+                {
+                    message = response.Message;
+                }
+            }
+            else
+            {
+                message = fields.Message;
+            }
+
+
+            return ProcessResults(output, message);
+        }
+
+        private void HandleModes(WebParse webParse)
+        {
+            HandleExpirables(provider.ExpirationDate, webParse.Expiration);
+            SetSanction(webParse.Sanction);
+
+            //try
+            //{
+            //    pdf.Html = html;
+            //    pdf.ConvertToImage("http://business.nh.gov/medicineboard/Search.aspx");
+            //}
+            //catch { }
+        }
+
+        private Result<string> Validate()
+        {
+
+            Boolean null_license = String.IsNullOrEmpty(provider.LicenseNumber);
+            Boolean pending_license = Regex.IsMatch(provider.LicenseNumber, "Pending", RegexOptions.IgnoreCase);
+            Boolean null_fname = String.IsNullOrEmpty(provider.FirstName);
+            Boolean null_lname = String.IsNullOrEmpty(provider.LastName);
+
+            if ( (null_license || pending_license) && (null_fname || null_lname) )
+            {
+                return Result<string>.Failure(ErrorMsg.InvalidLicenseAndFirstLastName);
+            }
+            else
+            {
+                return Result<string>.Success(String.Empty);
+            }
+        }
+
+    }
+}
---
+++ Work in Progress/WMHPLBPlugIn/WMHPLBPlugIn/WebSearch.cs
@@ -0,0 +1,101 @@
+﻿using PlugIn4_5;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace WMHPLBPlugIn
+{
+    public class WebSearch
+    {
+        // google sheets api
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "WMHPLBPlugIn";
+
+        private RegexOptions RegOpt = RegexOptions.IgnoreCase | RegexOptions.Singleline;
+        private Provider provider { get; set; }
+
+        public WebSearch(Provider _provider)
+        {
+            this.provider = _provider;
+        }
+
+        public Result<ValueRange> Execute()
+        {
+            try
+            {
+                return Search();
+            }
+            catch (Exception ex)
+            {
+                return Result<ValueRange>.Exception(ex);
+            }
+        }
+
+        private Result<ValueRange> Search()
+        {
+
+            /*----------------------------------------------------------------------------------------------------*/
+            /*------------------------------------------ GOOGLE SHEETS -------------------------------------------*/
+            /*----------------------------------------------------------------------------------------------------*/
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "1HdBP5KsCmmy1VuYTU8LZZI7RKUZ1aW4sFRjsILPYWXE";
+            String range = "Sheet1!A5:H";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+
+            // GET all values from spreadsheet
+            ValueRange response = request.Execute();
+
+            return Result<ValueRange>.Success(response);
+
+        }
+
+
+            /*----------------------------------------------------------------------------------------------------*/
+            /*------------------------------------------ GOOGLE SHEETS -------------------------------------------*/
+            /*----------------------------------------------------------------------------------------------------*/
+   
+
+        }
+}
